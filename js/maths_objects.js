4352177ff40e5bc0ab82fb96d5f4ccc2f1658fb2
Irfan <irfansk.se@gmail.com>
2021-03-11 11:19:55 +0530

    Numbers and Math Objects

---
+++ js/maths_objects.js
@@ -0,0 +1,28 @@
+const num1 = 100;
+const num2 = 50;
+
+let val;
+
+//Basic maths with numbers
+val = num1 + num2;
+val = num1 - num2;
+val = num1 * num2;
+val = num1 / num2;
+val = num1 % num2;
+
+//Math Object
+val = Math.PI;
+val = Math.E;   //Euler's Number
+val = Math.round(2.4);
+val = Math.ceil(2.3);
+val = Math.floor(2.6);
+val = Math.sqrt(64);
+val = Math.abs(-3423);
+val = Math.pow(2,4);
+val = Math.min(324, 5343, 5, 23, -3);
+val = Math.max(234, 4534, 54, 354, 55555);
+val = Math.random();
+
+val = Math.floor(Math.random() * 20);
+
+console.log(val);
