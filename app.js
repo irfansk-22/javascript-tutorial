2aeddbdf1b3c76cae9588e9df132ff32a2885298
Irfan <irfansk.se@gmail.com>
2021-03-11 09:49:47 +0530

    Using the console

---
+++ app.js
@@ -0,0 +1,38 @@
+//Log to console
+
+console.log("Hello, JS!");
+console.log(123);
+console.log(true);
+
+var greeting = 'Hello';
+console.log(greeting);
+
+
+console.log([43, 544, 54]);
+console.log({a:1, b:2});
+console.table({a: 32, b: 43});
+
+console.error("This is an error");
+console.warn("This is an warning");
+console.clear();
+
+console.time("Hello");
+  console.log("some text");
+  console.log("some text");
+  console.log("some text");
+  console.log("some text");
+  console.log("some text");
+  console.log("some text");
+  console.log("some text");
+  console.log("some text");
+console.timeEnd("Hello");
+
+//In console.time and console.timeEnd "Hello" is a identifier that 
+//should be same, the identifier can be anything but should be same.
+
+
+/*
+multi
+line
+comments
+*/
